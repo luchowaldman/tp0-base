49fd808ac2bc1823e89ac007b04ead29cb15397f
luchowaldman <lwaldman@fi.uba.ar>
2023-09-07 17:06:16 -0300

    Agrego final graceful para cliente en python

--- client/common/transmicioncontroller.py
+++ client/common/transmicioncontroller.py
@@ -1,12 +1,27 @@
 import logging
 import time
 from common.apuesta import Apuesta
-
+import signal
 
 class TransmicionController:
     def __init__(self, client):
         self.client = client
         self.ganadores = []
+        self._running = True
+
+        # Register a signal handler for SIGTERM
+        signal.signal(signal.SIGTERM, self.handle_sigterm)
+
+
+    def handle_sigterm(self, signum, frame):
+        """
+        Signal handler for SIGTERM
+
+        This method will be called when the server receives a SIGTERM signal.
+        It sets the _running flag to False, which will gracefully terminate the server.
+        """
+        logging.info("Received SIGTERM signal. Gracefully shutting down...")
+        self._running = False
 
     def TrasmitirArchivo(self, archivo_apuestas, apuestas_por_envio):
         self.client.conectar()
@@ -26,7 +41,10 @@ class TransmicionController:
                         if len(apuestas) >= apuestas_por_envio:
                             self.client.enviar_apuestas(apuestas)
                             apuestas = []  # Vaciar el vector de apuestas después de enviarlas
-                self.client.enviar_apuestas(apuestas)
+                    if not self._running:
+                        break            
+                if not self._running:    
+                    self.client.enviar_apuestas(apuestas)
             resultado_finapuestas = self.client.enviar_finapuestas()
         except Exception as e:
             logging.error(e)
@@ -43,7 +61,7 @@ class TransmicionController:
         #Voy a buscar ganadores. Si aun no estan espero un tiempo expotencial hasta que esten
         tiempo_espera = 1
         resultado_obtenido = False
-        while not resultado_obtenido:
+        while not resultado_obtenido and self._running:
             self.client.conectar()
             resultado_obtenido = self.client.enviar_consultaganador()
             self.client.cerrar_conexion()
