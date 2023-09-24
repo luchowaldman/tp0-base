ca6b0c187939e47bb71e1689d4cb671acc0ca2da
luchowaldman <lwaldman@fi.uba.ar>
2023-09-24 18:55:26 -0300

    Pruebas y pruebas en docker

--- testServer/testserverendock.sh
+++ testServer/testserverendock.sh
@@ -1,34 +1,13 @@
 #!/bin/bash
 
-# Nombre de la imagen del contenedor
-IMAGE_NAME="server"  # Cambia esto al nombre de tu imagen Docker
-
 # Puerto en el que se ejecuta tu servidor en el contenedor
 CONTAINER_PORT=12346
 
-# Buscar el contenedor por el nombre de la imagen
-CONTAINER_ID=$(docker ps -qf "ancestor=$IMAGE_NAME")
-
 TEST_STRING="Texto_de_Prueba"  
-# Verificar si se encontró el contenedor
-if [ -z "$CONTAINER_ID" ]; then
-  echo "No se encontró ningún contenedor Docker con la imagen '$IMAGE_NAME' en ejecución."
-  exit 1
-else
-  echo "Si encontro" $CONTAINER_ID
-fi
 
-# Obtener la dirección IP del contenedor
-CONTAINER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINER_ID)
+CONTAINER_IP="server"
 
-# Verificar si el contenedor Docker está en ejecución
-if [ -z "$CONTAINER_IP" ]; then
-  echo "No se pudo obtener la dirección IP del contenedor Docker."
-  exit 1
-else
-  echo "Servidor corriendo en" $CONTAINER_IP
-fi
-
+echo "Iniciando consulta"
 
 
 # Intentar conectarse al servidor en el contenedor utilizando netcat
@@ -53,6 +32,6 @@ if nc -z -v -w 5 "$CONTAINER_IP" "$CONTAINER_PORT"; then
   fi
 
 else
-  echo "No se pudo conectar al servidor en el contenedor Docker con la imagen '$IMAGE_NAME' ($CONTAINER_IP) en el puerto $CONTAINER_PORT."
+  echo "No se pudo conectar al servidor "
   exit 1
 fi
